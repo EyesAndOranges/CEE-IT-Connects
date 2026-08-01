3a99d6994badedbcf6b25741f776310da9ca19a6
Elise-JL <lacson.janaelise@gmail.com>
2026-08-01 18:31:26 +0800

    first

--- CEE_IT_Connects/PHP/password-one-time-reset.php
+++ CEE_IT_Connects/PHP/password-one-time-reset.php
@@ -1,22 +1,37 @@
 <?php
 require 'db.php';
 
-$stmt = $pdo->query("SELECT id, password_hash FROM advisers");
-$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// $stmt = $pdo->query("SELECT id, password_hash FROM advisers");
+// $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-foreach ($users as $user) {
+// foreach ($users as $user) {
 
-    $plainPassword = $user['password_hash'];
+//     $plainPassword = $user['password_hash'];
 
-    // Skip only if already hashed
-    if (strpos($plainPassword, '$2y$') === 0) {
-        continue;
-    }
+//     // Skip only if already hashed
+//     if (strpos($plainPassword, '$2y$') === 0) {
+//         continue;
+//     }
 
-    $hashed = password_hash($plainPassword, PASSWORD_DEFAULT);
+//     $hashed = password_hash($plainPassword, PASSWORD_DEFAULT);
 
-    $update = $pdo->prepare("UPDATE advisers SET password_hash = ? WHERE id = ?");
-    $update->execute([$hashed, $user['id']]);
-}
+//     $update = $pdo->prepare("UPDATE advisers SET password_hash = ? WHERE id = ?");
+//     $update->execute([$hashed, $user['id']]);
+// }
+
+// // echo "Passwords updated!";
+
+$id = 1;
+$newPassword = "12345";
+
+$hashed = password_hash($newPassword, PASSWORD_DEFAULT);
+
+$update = $pdo->prepare("
+    UPDATE students
+    SET password_hash = ?
+    WHERE id = ?
+");
+
+$update->execute([$hashed, $id]);
 
 echo "Passwords updated!";
